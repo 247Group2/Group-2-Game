7bfde282094737347504521b08d96c725789b1ab
kevinshieldsjr <amstineproject@gmail.com>
2022-09-08 11:16:22 -0400

    Created two classes (classEnemy and Display) KS (11/8/22) Added array list to classEnemy KS (11/8/22)

--- src/classEnemy.java
+++ src/classEnemy.java
@@ -1,2 +1,18 @@
 public class classEnemy {
+
+    public static void classEnemies() {
+
+        String[] classArray = new String[4];
+        classArray[0] = "Goblin";
+        classArray[1] = "Shrek";
+        classArray[2] = "Dragon";
+        classArray[3] = "Bob the Builder";
+
+        System.out.println("Choose Your Class (Enter Number) ");
+        System.out.println("1. " + classArray[0]);
+        System.out.println("2. " + classArray[1]);
+        System.out.println("3. " + classArray[2]);
+        System.out.println("4. " + classArray[3]);
+        System.out.println("-------------------");
+    }
 }
