29011cfc0a58a15c077fd1b958984a64991c1df1
kevinshieldsjr <amstineproject@gmail.com>
2022-09-08 11:10:41 -0400

    Created two classes (classEnemy and Display)

---
+++ src/Display.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Display {
+
+
+    public static void lineClass() {
+
+        //Scanner displayLine = new Scanner(System.in);
+        String line1 = new String(new char[23]).replace('\0', '-');
+    }
+}
--- src/classWeapons.java
+++ src/classWeapons.java
@@ -3,6 +3,9 @@ public class classWeapons {
 
     public static void classWeapon() {
 
+        /*Display weaponDisplay = new Display();
+        weaponDisplay.lineClass();*/
+
         String[] classArray = new String[4];
         classArray[0] = "Sword";
         classArray[1] = "Staff";
@@ -14,13 +17,12 @@ public class classWeapons {
         System.out.println("2. " + classArray[1]);
         System.out.println("3. " + classArray[2]);
         System.out.println("4. " + classArray[3]);
-        System.out.println("-------------------");
+       // System.out.println(Display.lineClass());
 
         Scanner classSelector = new Scanner(System.in);
         String chooseClass = classSelector.nextLine();
         if (chooseClass.equals("1")) {
-            System.out.println("-------------------");
-            System.out.println("You have selected " + classArray[0]);
+            System.out.println("\nYou have selected " + classArray[0] + "\n");
             System.out.println("-------------------");
         }else if (chooseClass.equals("2")) {
             System.out.println("-------------------");
